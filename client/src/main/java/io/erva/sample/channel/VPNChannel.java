80f14b7d04bec8c202f2a9149c9b4540080196c2
m.suchkov <maxim.suchkov@sigma.software>
2019-12-05 13:45:59 +0200

    Removed Channel name, added methods description

--- client/src/main/java/io/erva/sample/channel/VPNChannel.java
+++ client/src/main/java/io/erva/sample/channel/VPNChannel.java
@@ -1,19 +1,56 @@
 package io.erva.sample.channel;
 
 public interface VPNChannel {
-    String CHANNEL = "com.sigmasoftware.openVPN";
 
+
+    /**
+     * available VPN Status
+     */
+    enum OpenVPNStatus { connected, disconnected, invalid }
+
+    /**
+     * Check if OVPN configuration settled
+     * @return true if has configuration, false if not
+     */
     boolean isConfigurationExist();
 
+    /**
+     * Check connection status
+     *
+     * @return one of statuses form OpenVPNStatus
+     */
     String openVPNStatus();
 
+    /**
+     * Starts VPN session and save credentials for further connections
+     *
+     * @param username - login for vpn connection
+     * @param password - password for vpn connection
+     * @return true if vpn session started
+     */
     boolean setupAndStartVPN(String username, String password);
 
+    /**
+     * Starts VPN connection with stored credentials
+     * @return true if vpn session started
+     */
     boolean startVPN();
 
+    /**
+     * Stop vpn without closing session
+     */
     void stopVPN();
 
+    /**
+     * Stop Vpn and close session and remove credentials
+     * @return true if vpn session closed successfully and credentials removed
+     */
     boolean closeVPN();
 
+    /**
+     * Called when VPN status changes
+     *
+     * @return one of statuses form OpenVPNStatus
+     */
     String openVPNStatusDidChanged();
 }
